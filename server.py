1c81cea5dd485055f1e24ef9c5b9a1950ca66ded
Michael Negraru <negraru96@gmail.com>
2019-02-23 23:57:20 -0800

    Add files via upload
    Postform Method project. Displaying form info via Terminal library.

--- server.py
+++ server.py
@@ -1,13 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request, redirect
 app = Flask(__name__)
+
 @app.route('/')
 def index():
-    student_info = (
-        {'name' : 'Jim', 'age' : 35},
-        {'name' : 'Tim', 'age' : 30},
-        {'name' : 'Lim', 'age' : 25},
-        {'name' : 'Kim', 'age' : 30},
-    );
-    return render_template("index.html", random_numbers = [3,1,5,7,4], students = student_info)
-if __name__=="__main__"
+    return render_template("index.html")
+
+@app.route('/users', methods=['POST'])
+def create():
+    print(request.form)
+    print('Name', request.form['name'])
+    print('Email', request.form['Email'])
+    return render_template("created.html")
+
+if __name__=="__main__":
     app.run(debug=True)
