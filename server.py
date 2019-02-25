adcc9632b51c965316574c4205f2ad933b4f87b0
Michael Negraru <negraru96@gmail.com>
2019-02-24 22:49:55 -0800

    Add files via upload
    Dojo Survey assignment. Collecting form data and displaying it via terminal. I made my project a Californian residential activity form

--- server.py
+++ server.py
@@ -5,12 +5,20 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/users', methods=['POST'])
+@app.route('/results', methods=['POST'])
 def create():
     print(request.form)
-    print('Name', request.form['name'])
+    print('Name', request.form['Name'])
     print('Email', request.form['Email'])
+    print('City', request.form['City'])
+    print('Activity', request.form['Activity'])
+    print('Other', request.form['Other'])
     return render_template("created.html")
+
+@app.route('/danger')
+def danger():
+    return redirect('/')
+    alert("A user tried to visit /danger. We have redirected the user to /");
 
 if __name__=="__main__":
     app.run(debug=True)
